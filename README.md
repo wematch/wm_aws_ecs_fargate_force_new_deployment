e82f7600b8d425e5dd2743da05807c93067ee9e3
Dmitriy Solodukha <dspv@me.com>
2022-12-22 14:00:47 +0200

    Document + Cleanup (#2)

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# wm_aws_ecs_fargate_force_new_deployment
+## Requirements
+
+No requirements.
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | n/a |
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_lambda"></a> [lambda](#module\_lambda) | terraform-aws-modules/lambda/aws | ~> 2.0 |
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_iam_policy.policy](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy) | resource |
+| [aws_region.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/region) | data source |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_account"></a> [account](#input\_account) | n/a | `any` | n/a | yes |
+| <a name="input_cloudwatch_logs_retention_in_days"></a> [cloudwatch\_logs\_retention\_in\_days](#input\_cloudwatch\_logs\_retention\_in\_days) | n/a | `number` | `3` | no |
+| <a name="input_ecs_cluster"></a> [ecs\_cluster](#input\_ecs\_cluster) | n/a | `string` | n/a | yes |
+| <a name="input_name_prefix"></a> [name\_prefix](#input\_name\_prefix) | n/a | `string` | n/a | yes |
+| <a name="input_schedule"></a> [schedule](#input\_schedule) | Schedule rule for Lambda | `map` | <pre>{<br>  "description": "Fires Lambda every Sunday morning at 7 UTC",<br>  "expression": "cron(0 7 ? * SUN *)",<br>  "name": "run-every-week"<br>}</pre> | no |
+| <a name="input_standard_tags"></a> [standard\_tags](#input\_standard\_tags) | n/a | `map(string)` | n/a | yes |
+
+## Outputs
+
+No outputs.
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data aws_region current {}
 
 module lambda {
     source  = "terraform-aws-modules/lambda/aws"
-    version = "~> 2.0"
+    version = "~> 2.0" # TODO: Update module version
 
     function_name                       = "${var.name_prefix}-ECS-Force-New-Deployment"
     description                         = "ECS Force New Deployment for ${var.name_prefix}"
@@ -28,7 +28,8 @@ resource aws_iam_policy policy {
     path        = "/"
     description = "Allow to list and update ECS services withing the ${var.ecs_cluster} cluster"
 
-    ### Policy looks complicated? Here's why: https://stackoverflow.com/questions/59604987/why-i-am-getting-not-authorized-to-perform-ecslisttasks-on-resource-excep
+    # Policy looks complicated? Here's why: 
+    # https://stackoverflow.com/questions/59604987/why-i-am-getting-not-authorized-to-perform-ecslisttasks-on-resource-excep
     policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -49,22 +50,3 @@ resource aws_iam_policy policy {
         ]
     })
 }
-
-# resource aws_cloudwatch_event_rule schedule {
-#     name                = var.schedule.name
-#     description         = var.schedule.description
-#     schedule_expression = var.schedule.expression
-# }
-
-# resource aws_cloudwatch_event_target this {
-#     rule = aws_cloudwatch_event_rule.schedule.name
-#     arn  = module.lambda.lambda_function_arn
-# }
-
-# resource aws_lambda_permission allow_cloudwatch_to_invoke_lambda {
-#     statement_id = "AllowExecutionFromCloudWatch"
-#     action = "lambda:InvokeFunction"
-#     function_name = module.lambda.lambda_function_name
-#     principal = "events.amazonaws.com"
-#     source_arn = aws_cloudwatch_event_rule.schedule.arn
-# }
